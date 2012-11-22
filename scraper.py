6178dded3c82295db88e412061395ede70a510ad
Zarino Zappia <mail@zarino.co.uk>
2012-11-22 22:24:13 +0000

    now scrapes user.getInfo as well as user.getRecentTracks

--- scraper.py
+++ scraper.py
@@ -117,11 +117,51 @@ def getRecentTracks():
             continue
 
 
+def getInfo():
+    print "Scraping %s.getInfo..." % user
+
+    dt.execute("CREATE TABLE IF NOT EXISTS info (date INT, user TEXT, id INT, realname TEXT, url TEXT, image TEXT, country TEXT, age INT, gender TEXT, subscriber INT, playcount INT, playlists INT, bootstrap INT, registered INT)")
+    dt.create_index(['date','user'], 'info', unique=True)
+    
+    params = {
+        'method': 'user.getinfo',
+        'user': user,
+        'api_key': api_key
+    }
+    req = requests.get('http://ws.audioscrobbler.com/2.0/', params=params)
+    xml = req.text.replace(' encoding="utf-8"', '') # Stop lxml complaining about encodings
+    dom = lxml.html.fromstring(xml)
+    
+    if dom.cssselect('error'):
+        raise Exception(dom.cssselect('error')[0].text)
+        exit()
+    
+    dt.upsert({
+        'date': now,
+        'user': user,
+        'id': dom.cssselect('id')[0].text,
+        'realname': dom.cssselect('realname')[0].text,
+        'url': dom.cssselect('url')[0].text,
+        'image': dom.cssselect('image[size="extralarge"]')[0].text,
+        'country': dom.cssselect('country')[0].text,
+        'age': dom.cssselect('age')[0].text,
+        'gender': dom.cssselect('gender')[0].text,
+        'subscriber': dom.cssselect('subscriber')[0].text,
+        'playcount': dom.cssselect('playcount')[0].text,
+        'playlists': dom.cssselect('playlists')[0].text,
+        'bootstrap': dom.cssselect('bootstrap')[0].text,
+        'registered': dom.cssselect('registered')[0].get('unixtime')
+    }, "info")
+    print '  All done!'
+
+
 now = int(time.time())
 dt = dumptruck.DumpTruck(dbname="lastfm.sqlite")
 
 print '----- START %s -----' % time.strftime('%Y-%m-%d %H:%I:%S', time.gmtime())
 
 getRecentTracks()
+
+getInfo()
 
 print '----- END %s -----' % time.strftime('%Y-%m-%d %H:%I:%S', time.gmtime())
